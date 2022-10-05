b1d54665596742c0e7469f37a5e4cf06a0652b18
suvenrj <99804277+suvenrj@users.noreply.github.com>
2022-10-05 21:53:45 +0530

    Initial Commit

---
+++ db_connection_php.php
@@ -0,0 +1,84 @@
+
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "password";
+
+	$database = "new_schema";
+
+	// Create a connection
+	$conn = mysqli_connect($servername,
+		$username, $password, $database);
+
+	// Code written below is a step taken
+	// to check that our Database is
+	// connected properly or not. If our
+	// Database is properly connected we
+	// can remove this part from the code
+	// or we can simply make it a comment
+	// for future reference.
+
+	if($conn) {
+		echo "success";
+	}
+	else {
+		die("Error". mysqli_connect_error());
+	}
+?>
+
+
+
+
+
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
+
+
